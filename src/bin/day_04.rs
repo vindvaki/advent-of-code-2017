94df12ce8a60b6052b7a7e090210ee9e179bcebb
Hordur Freyr Yngvason <hordurf@gmail.com>
2017-12-04 20:04:12 +0100

    Day 04

---
+++ src/bin/day_04.rs
@@ -0,0 +1,78 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let path = args.get(1).expect("fixture path must be provided");
+    let mut file = File::open(path).expect("unable to open file");
+    let mut data = String::new();
+    file.read_to_string(&mut data).expect("unable to read file");
+
+    part_1(&data.as_str());
+    part_2(&data.as_str());
+}
+
+fn part_1(data: &str) {
+    let solution = data.lines().filter(|&line| !contains_duplicate_words(line)).count();
+    println!("part_1: {}", solution);
+}
+
+fn part_2(data: &str) {
+    let solution = data.lines().filter(|&line| !contains_anagrams(line)).count();
+    println!("part_2: {}", solution);
+}
+
+fn contains_duplicate_words(line: &str) -> bool {
+    let mut seen = std::collections::HashSet::new();
+    for word in line.split_whitespace() {
+        if seen.contains(word) {
+            return true;
+        }
+        seen.insert(word);
+    }
+    false
+}
+
+fn contains_anagrams(line: &str) -> bool {
+    let mut seen = std::collections::HashSet::new();
+    for word in line.split_whitespace() {
+        let sorted = {
+            let mut res: Vec<char> = word.chars().collect();
+            res.sort();
+            res
+        };
+        if seen.contains(&sorted) {
+            return true;
+        }
+        seen.insert(sorted);
+    }
+    false
+}
+
+#[cfg(test)]
+mod tests {
+    use contains_anagrams;
+
+    #[test]
+    fn test_contains_anagrams_with_anagrams() {
+        assert_eq!(contains_anagrams("abc r cba b d"), true);
+    }
+
+    #[test]
+    fn test_contains_anagrams_without_anagrams() {
+        assert_eq!(contains_anagrams("aabc r cba b d"), false);
+    }
+
+    use contains_duplicate_words;
+
+    #[test]
+    fn test_contains_duplicate_words_with_duplicates() {
+        assert_eq!(contains_duplicate_words("a abc b abc"), true);
+    }
+
+    #[test]
+    fn test_contains_duplicate_words_without_duplicates() {
+        assert_eq!(contains_duplicate_words("a b c"), false);
+    }
+}
